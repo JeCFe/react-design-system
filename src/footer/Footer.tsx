eb97540e16f13d7de50efc0b5b6cb8160efcc408
Jessica Fealy <jessicaf091@outlook.com>
2023-12-26 00:35:52 +0000

    Added footer

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+type Props = {
+  children?: ReactNode | ReactNode[];
+};
+
+export const Footer = ({ children }: Props) => {
+  return (
+    <div className="flex items-center w-screen h-auto p-4 bg-white shadow-[0_-25px_50px_-12px_rgba(0,0,0,0.25)] shadow-pink-200 mt-20 text-xl text-gray-400">
+      <div className="container mx-auto flex justify-center">{children}</div>
+    </div>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import "./tailwind.css";
 export { Button } from "./button";
 export { Accordion } from "./accordion";
 export { Checkbox } from "./checkbox";
+export { Footer } from "./footer";
