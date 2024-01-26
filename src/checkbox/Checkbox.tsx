182ea295eec802c3048f5aa16c6f270651f19d60
Jessica Fealy <jessicaf091@outlook.com>
2024-01-26 22:15:05 +0000

    Updated checkbox

--- src/checkbox/Checkbox.tsx
+++ src/checkbox/Checkbox.tsx
@@ -8,6 +8,7 @@ const checkbox = cva(
     "appearance-none box-border relative border-2 border-black",
     "focus:before:absolute focus:before:-left-0.5 focus:before:-top-0.5 focus:before:border-4 focus:before:border-black focus:before:ring focus:before:ring-pink-400",
     "checked:after:absolute checked:after:box-border checked:after:rotate-45 checked:after:border-solid checked:after:border-pink-400",
+    "disabled:bg-gray-300",
   ],
   {
     variants: {
