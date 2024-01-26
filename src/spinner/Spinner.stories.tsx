8d0df83bd7cdc10c34fd2a7eb8fdd817a10cf933
Jessica Fealy <jessicaf091@outlook.com>
2024-01-26 22:12:18 +0000

    Updated spinner

--- src/spinner/Spinner.stories.tsx
+++ src/spinner/Spinner.stories.tsx
@@ -1,6 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Spinner } from "./Spinner";
+import { ComponentProps } from "react";
+import React from "react";
+
+const sizes: ComponentProps<typeof Spinner>["size"][] = [
+  "xsmall",
+  "small",
+  "medium",
+  "large",
+];
 
 const meta: Meta<typeof Spinner> = {
   title: "Component/Spinner",
@@ -11,12 +20,25 @@ const meta: Meta<typeof Spinner> = {
 export default meta;
 type Story = StoryObj<typeof Spinner>;
 
-export const Basic: Story = {
+export const Default: Story = {
   args: {},
 };
 
-export const Fast: Story = {
-  args: {
-    fast: true,
+export const Variants: Story = {
+  render: () => {
+    return (
+      <div className="flex flex-row space-x-8">
+        {sizes.map((size) => (
+          <div className="flex flex-col">
+            <h1 className="text-xl font-bold pb-4">{size}</h1>
+            <div className="flex flex-col space-y-4">
+              {[false, true].map((fast) => (
+                <Spinner size={size} fast={fast} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   },
 };
