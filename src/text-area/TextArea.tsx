775e6591c8d23adb4537097c970965b6bae7a5ca
Jessica Fealy <jessicaf091@outlook.com>
2024-01-28 16:09:04 +0000

    text area

---
+++ src/text-area/TextArea.tsx
@@ -0,0 +1,95 @@
+import { VariantProps, cva } from "class-variance-authority";
+// @ts-ignore
+import React, {
+  ChangeEvent,
+  TextareaHTMLAttributes,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+const textArea = cva(
+  [
+    "appearance-none border-2 p-3 pb-4 border-black rounded-md resize-none min-w-40",
+    "focus:outline-none focus:ring-2 focus:ring-pink-400",
+    "disabled:bg-gray-300",
+  ],
+  {
+    variants: {
+      width: {
+        medium: "w-96",
+        large: "w-[500px]",
+        full: "w-full",
+      },
+      height: {
+        small: "h-40",
+        medium: "h-60",
+        large: "h-[500px]",
+        full: "h-full",
+      },
+    },
+    defaultVariants: {
+      width: "full",
+      height: "medium",
+    },
+  },
+);
+
+type Props = {
+  autoGrow?: boolean;
+  onChange?: (text: string) => void;
+} & Omit<
+  TextareaHTMLAttributes<HTMLTextAreaElement>,
+  "onChange" | "width" | "height"
+> &
+  VariantProps<typeof textArea>;
+
+export const TextArea = forwardRef<HTMLTextAreaElement, Props>(
+  (
+    {
+      autoGrow = false,
+      onChange = () => {},
+      className,
+      width,
+      height,
+      ...rest
+    }: Props,
+    forwardedRef,
+  ) => {
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    const setAutoHeight = () => {
+      if (autoGrow && textAreaRef.current) {
+        textAreaRef.current.style.height = "0px";
+        const scrollHeight = textAreaRef.current!.scrollHeight;
+        textAreaRef.current.style.height = scrollHeight + "px";
+      }
+    };
+
+    useEffect(() => {
+      setAutoHeight();
+    }, [autoGrow]);
+
+    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      setAutoHeight();
+
+      onChange(event.target.value);
+    };
+
+    useImperativeHandle(forwardedRef, () => textAreaRef.current!, [
+      textAreaRef,
+    ]);
+
+    return (
+      <div>
+        <textarea
+          ref={textAreaRef}
+          onChange={handleChange}
+          className={textArea({ width, height, className })}
+          {...rest}
+        />
+      </div>
+    );
+  },
+);
---
+++ src/text-area/text-area.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import React, { ComponentProps, useRef } from "react";
+import { TextArea } from "./TextArea";
+
+const widths: ComponentProps<typeof TextArea>["width"][] = [
+  "medium",
+  "large",
+  "full",
+];
+const heights: ComponentProps<typeof TextArea>["height"][] = [
+  "small",
+  "medium",
+  "large",
+  "full",
+];
+
+const meta: Meta<typeof TextArea> = {
+  title: "Component/TextArea",
+  component: TextArea,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof TextArea>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Variants: Story = {
+  render: () => {
+    return (
+      <div className="flex w-full flex-row space-x-8">
+        {widths.map((width) => (
+          <div className="flex w-full flex-col">
+            <h1 className="pb-4 text-xl font-bold">{width}</h1>
+            <div className="flex w-full flex-col space-y-4">
+              <TextArea autoGrow width={width} placeholder={`${width} grow`} />
+              <TextArea
+                width={width}
+                placeholder={`${width} disabled`}
+                disabled
+              />
+
+              {heights.map((height) => {
+                return (
+                  <TextArea
+                    width={width}
+                    height={height}
+                    placeholder={`${width} ${height}`}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  },
+};
