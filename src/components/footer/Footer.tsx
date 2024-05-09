19c119971afaa157da12d9ede9df42a26d4740db
Jessica Fealy <jessicaf091@outlook.com>
2024-05-09 18:19:41 +0100

    Added example logo to the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,12 +3,20 @@ import React, { ReactNode } from "react";
 
 type Props = {
   children?: ReactNode | ReactNode[];
+  logo: ReactNode;
 };
 
-export const Footer = ({ children }: Props) => {
+export const Footer = ({ children, logo }: Props) => {
   return (
     <div className="mt-20 flex h-auto w-screen items-center bg-white p-4 text-xl text-gray-400 shadow-[0_-25px_50px_-12px_rgba(0,0,0,0.25)] shadow-pink-200">
-      <div className="container mx-auto flex justify-center">{children}</div>
+      <div className="container mx-auto flex flex-col">
+        <div>{children}</div>
+
+        <div className="flex">
+          <div className="flex flex-grow" />
+          {logo}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/footer/footer.stories.tsx
+++ src/components/footer/footer.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import React from "react";
+import { JecfeLogoBlack } from "../react-svg";
 import { Footer } from "./Footer";
 
 const meta: Meta<typeof Footer> = {
@@ -14,5 +16,6 @@ type Story = StoryObj<typeof Footer>;
 export const Default: Story = {
   args: {
     children: "Hello there :O",
+    logo: <JecfeLogoBlack height="32" />,
   },
 };
