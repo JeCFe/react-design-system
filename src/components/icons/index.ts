78ebbe0ff19a2343231a3511f1fb3def5d962045
Jessica Fealy <jessicaf091@outlook.com>
2024-05-09 18:04:18 +0100

    fix svgr bug

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,4 +2,7 @@ export { default as ArrowDown } from "./arrow-down.svg";
 export { default as ArrowUp } from "./arrow-up.svg";
 export { default as Delete } from "./delete.svg";
 export { default as InfoSvg } from "./info.svg";
+export { default as JecfeLogoBlack } from "./jecfe-logo-black.svg";
+export { default as JecfeLogoWhite } from "./jecfe-logo-white.svg";
+export { default as JecfeLogo } from "./jecfe-logo.svg";
 export { default as SpinnerSvg } from "./spinner.svg";
