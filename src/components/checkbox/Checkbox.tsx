8fa160fac61e6451cc18ca33db68320e954a79c4
Jessica Fealy <jessicaf091@outlook.com>
2024-11-16 23:47:45 +0000

    Updated checkboxes

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -4,10 +4,10 @@ import React, { InputHTMLAttributes, ReactNode, forwardRef } from "react";
 
 const checkbox = cva(
   [
-    "appearance-none box-border relative border-2 border-black",
-    "focus:before:absolute focus:before:-left-0.5 focus:before:-top-0.5 focus:before:border-4 focus:before:border-black focus:before:ring focus:before:ring-pink-400",
-    "checked:after:absolute checked:after:box-border checked:after:rotate-45 checked:after:border-solid checked:after:border-pink-400",
-    "disabled:bg-gray-300",
+    "appearance-none box-border relative border-2",
+    "focus:before:absolute focus:before:-left-0.5 focus:before:-top-0.5 focus:before:border-4 focus:before:ring",
+    "checked:after:absolute checked:after:box-border checked:after:rotate-45 checked:after:border-solid",
+    "disabled:bg-gray-300 hover:ring-2",
   ],
   {
     variants: {
@@ -22,6 +22,18 @@ const checkbox = cva(
           "focus:before:h-10 focus:before:w-10",
           "checked:after:left-3 checked:after:top-[3px] checked:after:h-[25px] checked:after:w-3 checked:after:border-b-[5px] checked:after:border-r-[5px]",
         ],
+        large: [
+          "h-12 min-h-12 w-12 min-w-12",
+          "focus:before:h-12 focus:before:w-12",
+          "checked:after:left-4 checked:after:top-[5px] checked:after:h-[30px] checked:after:w-3 checked:after:border-b-[5px] checked:after:border-r-[5px]",
+        ],
+      },
+      theme: {
+        standard:
+          "border-slate-900 focus:before:border-slate-900 after:checked:border-pink-400 focus:before:ring-pink-400 hover:ring-pink-400",
+        dark: "border-slate-400 focus:before:border-slate-400 after:checked:border-pink-400 focus:before:ring-pink-400 hover:ring-pink-400",
+        pink: "border-pink-500 focus:before:border-pink-500 after:checked:border-cyan-500 focus:before:ring-cyan-500 hover:ring-pink-500",
+        cyan: "border-cyan-500 focus:before:border-cyan-500 after:checked:border-pink-500 focus:before:ring-pink-500 hover:ring-cyan-500",
       },
     },
     defaultVariants: {
@@ -31,7 +43,13 @@ const checkbox = cva(
 );
 
 const label = cva(["flex flex-col w-fit"], {
-  variants: { size: { medium: "text-lg", small: "text-base" } },
+  variants: {
+    size: { medium: "text-lg", small: "text-base", large: "text-xl" },
+    darkMode: {
+      standard: "text-slate-800",
+      dark: "text-slate-300",
+    },
+  },
   defaultVariants: { size: "small" },
 });
 
@@ -40,21 +58,21 @@ type Props = {
   hint?: ReactNode;
   className?: string;
 } & Omit<InputHTMLAttributes<HTMLInputElement>, "size"> &
-  VariantProps<typeof checkbox>;
+  VariantProps<typeof checkbox> &
+  VariantProps<typeof label>;
 
 export const Checkbox = forwardRef<HTMLInputElement, Props>(
-  ({ children, hint, size, className, ...rest }: Props, ref) => (
-    <label className={label({ size, className })}>
+  ({ children, size, className, theme, darkMode, ...rest }: Props, ref) => (
+    <label className={label({ size, className, darkMode })}>
       <div className="flex w-fit flex-row items-center">
         <input
           {...rest}
-          className={checkbox({ size })}
+          className={checkbox({ size, theme })}
           ref={ref}
           type="checkbox"
         />
         <span className="ml-4">{children}</span>
       </div>
-      {hint && <span className="ml-12 text-gray-500">{hint}</span>}
     </label>
   ),
 );
