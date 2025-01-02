040e1cacabea7bdc2d5fba012e93e11403d85a05
Jessica Fealy <jessicaf091@outlook.com>
2025-01-02 21:33:02 +0000

    Actually export *

--- src/components/anchor/Anchor.tsx
+++ src/components/anchor/Anchor.tsx
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 // @ts-ignore
 import React, { AnchorHTMLAttributes, ReactNode } from "react";
 
-const anchorCva = cva([
+export const anchorCva = cva([
   "inline-block underline underline-offset-4",
   "text-blue-500 hover:text-blue-900",
   "transition duration-200 ease-in-out",
